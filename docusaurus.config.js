436e3fc82e4b2c16b56b3f8552a39dcfa9d01c64
Anıl Akdereli <25330888+anilakdereli@users.noreply.github.com>
2023-05-18 12:48:20 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+        /* other docs plugin options */
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
